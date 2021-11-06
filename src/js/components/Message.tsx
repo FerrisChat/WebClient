cb950ee318e985d12e4bcb68bb5f7bf8fb30f65f
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-11-05 22:29:44 -0400

    Switch from marked to markdown-it
    Adds support for __underline__

--- src/js/components/Message.tsx
+++ src/js/components/Message.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import marked from 'marked';
+import mdit from 'markdown-it';
 import DOMPurify from 'dompurify';
 import { MessageProps } from '../types';
+
+const md = mdit({ linkify: true });
+md.renderer.rules.strong_open = md.renderer.rules.strong_close = (tokens, index, options, _, self) => {
+    let token = tokens[index];
+    if (token.markup === '__') {
+        token.tag = 'u';
+    }
+    return self.renderToken(tokens, index, options);
+}
 
 export default function Message({ id, content }: MessageProps) {
     return (
         <div className='message' data-message-id={id}>
             <span className='message-content' dangerouslySetInnerHTML={
-                { __html: DOMPurify.sanitize(marked.parseInline(content.replace(/ {0,1}\n/g, "  \n"))) }
+                { __html: DOMPurify.sanitize(md.renderInline(content.replace(/ {0,1}\n/g, "  \n"))) }
             } />
         </div>
     )
