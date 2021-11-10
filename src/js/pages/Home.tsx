42009b4eadb807962415d729f5686928fe94fd23
MrKomodoDragon <svrchn921@outlook.com>
2021-11-09 16:33:37 -0800

    Make it so it says '1 guild' not '1 guilds' as it previously

--- src/js/pages/Home.tsx
+++ src/js/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <>
             <p>Hello, {window.api?.user?.name}!</p>
-            <p>You are in {window.api?.guilds?.length} guilds.</p>
+            <p>You are in {window.api?.guilds?.length} guild{window.api?.guilds?.length > 1|| window.api?.guilds?.length < 1 ? "s" : ""  }.</p>
         </>
     )
 }
