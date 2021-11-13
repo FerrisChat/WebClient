b9d62701e11e54b3a2d3a8d4ba370a21941cbe2e
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-11-12 21:44:35 -0500

    Assert not null rather than coalesce

--- src/js/pages/Home.tsx
+++ src/js/pages/Home.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     return (
         <CommonLayout>
             <div className="generic-message">
-                <h1>Hello, <b>{window.api?.user?.name}</b>!</h1>
-                <p>You are in {window.api?.guilds?.length} guild{window.api?.guilds?.length !== 1 ? "s" : ""}.</p>
+                <h1>Hello, <b>{window.api!.user?.name}</b>!</h1>
+                <p>You are in {window.api!.guilds!.length} guild{window.api!.guilds!.length !== 1 ? "s" : ""}.</p>
             </div>
         </CommonLayout>
 
