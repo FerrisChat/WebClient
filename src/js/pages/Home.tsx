c0bfd021e6514e165e1da495147d436b000e51c7
MrKomodoDragon <74436682+MrKomodoDragon@users.noreply.github.com>
2021-11-12 18:45:33 -0800

    Make it so it says '1 guild' not '1 guilds' (#4)
    * Make it so it says '1 guild' not '1 guilds' as it previously
    
    * Make it so it says '1 guild' not '1 guilds' as it previously said.... again
    
    * Assert not null rather than coalesce
    
    Co-authored-by: Jay3332 <40323796+jay3332@users.noreply.github.com>

--- src/js/pages/Home.tsx
+++ src/js/pages/Home.tsx
@@ -5,9 +5,10 @@ export default function Home() {
     return (
         <CommonLayout>
             <div className="generic-message">
-                <h1>Hello, <b>{window.api?.user?.name}</b>!</h1>
-                <p>You are in {window.api?.guilds?.length} guilds.</p>
+                <h1>Hello, <b>{window.api!.user?.name}</b>!</h1>
+                <p>You are in {window.api!.guilds!.length} guild{window.api!.guilds!.length !== 1 ? "s" : ""}.</p>
             </div>
         </CommonLayout>
+
     )
 }
