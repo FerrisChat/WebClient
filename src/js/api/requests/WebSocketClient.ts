0766bbc23d9c0005a298c841a38e485cbfaec58f
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-11-13 22:26:43 -0800

    Fix heartbeat (#6)
    * Fix heartbeat
    
    * typo

--- src/js/api/requests/WebSocketClient.ts
+++ src/js/api/requests/WebSocketClient.ts
@@ -115,7 +115,7 @@ export default class WebSocketClient {
     }
 
     sendHeartbeat() {
-        this.ws!.send("heartbeat")
+        this.ws!.sendJSON({c: 'Ping'})
     }
 
     sendJSON(json: any) {
