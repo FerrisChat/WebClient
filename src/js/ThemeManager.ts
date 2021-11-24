cb9cf8f74712b18cf2d525def4b49067eec4ab96
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-11-24 18:05:27 -0500

    Implement custom themes

--- src/js/ThemeManager.ts
+++ src/js/ThemeManager.ts
@@ -21,18 +21,38 @@ export default class ThemeManager {
     }
 
     resolveTheme() {
-        this.theme = THEME_MAPPING[this.themeName = Cookies.get('theme') || 'dark'];
+        this.theme = Object.assign({}, THEME_MAPPING[this.themeName = Cookies.get('theme') || 'dark']);
         this.updateTheme();
+    }
+
+    updateElements(k: string, v: any) {
+        const pickerElement = document.getElementById(`custom-theme-picker__${k}`);
+        // @ts-ignore
+        if (pickerElement) pickerElement.value = v;
+
+        const showElement = document.getElementById(`custom-theme-picker-color__${k}`);
+        // @ts-ignore
+        if (showElement) showElement.style.backgroundColor = v;
+
+        const textPickerElement = document.getElementById(`custom-theme-picker-text__${k}`);
+        // @ts-ignore
+        if (textPickerElement) textPickerElement.value = v;
     }
 
     updateTheme() {
         for (const [k, v] of Object.entries(this.theme)) {
             this._selector.style.setProperty('--' + k, v);
+            this.updateElements(k, v)
         }
     }
 
+    updateThemeFor(key: string) {
+        this._selector.style.setProperty('--' + key, this.theme[key]);
+        this.updateElements(key, this.theme[key])
+    }
+
     putPresetTheme(name: string) {
-        const theme = THEME_MAPPING[name = name.toLowerCase()];
+        const theme = Object.assign({}, THEME_MAPPING[name = name.toLowerCase()]);
         if (!theme) return;
 
         Cookies.set('theme', this.themeName = name);
--- src/js/pages/settings/Theme.tsx
+++ src/js/pages/settings/Theme.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { EventHandler, FormEvent, FormEventHandler } from 'react';
 import SettingsLayout from "./SettingsLayout";
 
 function PresetTheme({ label, name, description }: { label: string, name: string, description: string }) {
@@ -10,14 +10,60 @@ function PresetTheme({ label, name, description }: { label: string, name: string
     )
 }
 
+function CustomThemePicker({ label, name }: { label: string, name: string }) {
+    const onInput = (e: FormEvent<HTMLInputElement>) => {
+        // @ts-ignore
+        window.themeManager.theme[name] = e.target.value;
+        window.themeManager.updateThemeFor(name)
+    }
+
+    let current = window.themeManager.theme[name];
+    return (
+        <div className='custom-theme-picker'>
+            <input type='color' id={`custom-theme-picker__${name}`} name={name} value={current} onInput={onInput} />
+            <div className='custom-theme-picker-color' id={`custom-theme-picker-color__${name}`} onClick={
+                () => document.getElementById(`custom-theme-picker__${name}`)?.click()
+            } style={{backgroundColor: current}} />
+            <div className='custom-theme-picker-content'>
+                <div>{label}</div>
+                <input id={`custom-theme-picker-text__${name}`} type='text' placeholder={current} onChange={onInput}  />
+            </div>
+        </div>
+    )
+}
+
 export default function Theme() {
     return (
         <SettingsLayout>
             <div className="themes">
+                <h1>Themes</h1>
                 <div className="preset-themes">
+                    <h2>Preset Themes</h2>
                     <PresetTheme label='Light' name='light' description='A light theme with bright, contrasted colors.' />
                     <PresetTheme label='Dark' name='dark' description='The default theme: A dark theme with moderately contrasted colors.' />
                     <PresetTheme label='Midnight' name='midnight' description='A pitch, pure black theme with contrast in text. Darker than dark mode.' />
+                </div>
+                <div className="custom-theme">
+                    <h2>Custom Theme</h2>
+                    <div className="custom-theme-pickers">
+                        <CustomThemePicker label="Background (Primary)" name="dark" />
+                        <CustomThemePicker label="Background (Secondary/Chat)" name="darker" />
+                        <CustomThemePicker label="Background (Tertiary)" name="darkest" />
+                        <CustomThemePicker label="Background (Quaternary)" name="pitch" />
+                        <CustomThemePicker label="Generic (Primary)" name="primary" />
+                        <CustomThemePicker label="Generic (Secondary)" name="secondary" />
+                        <CustomThemePicker label="Text/Foreground" name="text" />
+                        <CustomThemePicker label="Text/Foreground (Error)" name="error" />
+                        <CustomThemePicker label="Link" name="link" />
+                        <CustomThemePicker label="Link (Hover)" name="link-hover" />
+                        <CustomThemePicker label="Scrollbar Thumb" name="scrollbar-front" />
+                        <CustomThemePicker label="Scrollbar Thumb (Hover)" name="scrollbar-front-hover" />
+                        <CustomThemePicker label="Scrollbar Track" name="scrollbar-back" />
+                        <CustomThemePicker label="Button (Primary)" name="button-primary" />
+                        <CustomThemePicker label="Button (Secondary)" name="button-secondary" />
+                        <CustomThemePicker label="Button (Success)" name="button-success" />
+                        <CustomThemePicker label="Button (Danger)" name="button-danger" />
+                    </div>
                 </div>
             </div>
         </SettingsLayout>
